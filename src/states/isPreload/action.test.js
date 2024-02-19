ce25f7e723e848625b9d522fdc31c437d7549c7e
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-02-19 23:47:07 +0700

    test: added test scenarios for asyncPreloadProcess thunk
    - Test scenarios for asyncPreloadProcess thunk:
      - Dispatches actions correctly when data fetching succeeds
      - Dispatches actions and handles alert correctly when data fetching fails

---
+++ src/states/isPreload/action.test.js
@@ -0,0 +1,71 @@
+/**
+ * skenario test
+ *
+ * - asyncPreloadProcess thunk
+ *  - should dispatch action correctly when data fetching success
+ *  - should dispatch action and call alert correctly when data fetching failed
+ */
+
+import {
+  afterEach, beforeEach, describe, expect, it, vi,
+} from 'vitest';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { asyncPreloadProcess, setIsPreloadActionCreator } from './action';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+const fakeUsersResponse = {
+  id: 'john_doe',
+  name: 'John Doe',
+  email: 'john@example.com',
+  avatar: 'https://generated-image-url.jpg',
+};
+
+const fakeErrorResponse = new Error('Ups, something went wrong');
+
+describe('asyncReceiveLeaderboards thunk', () => {
+  beforeEach(() => {
+    api._getOwnProfile = api.getOwnProfile;
+  });
+
+  afterEach(() => {
+    api.getOwnProfile = api._getOwnProfile;
+
+    // delete backup data
+    delete api._getOwnProfile;
+  });
+
+  it('should dispatch action correctly when data fetching success', async () => {
+    // arrange
+    // stub implementation
+    api.getOwnProfile = () => Promise.resolve(fakeUsersResponse);
+    // mock dispatch
+    const dispatch = vi.fn();
+
+    // action
+    await asyncPreloadProcess()(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator(fakeUsersResponse));
+    expect(dispatch).toHaveBeenCalledWith(setIsPreloadActionCreator(false));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+
+  it('should dispatch action and call alert correctly when data fetching failed', async () => {
+    // arrange
+    // stub implementation
+    api.getOwnProfile = () => Promise.reject(fakeErrorResponse);
+    // mock dispatch
+    const dispatch = vi.fn();
+
+    // action
+    await asyncPreloadProcess()(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(setAuthUserActionCreator(null));
+    expect(dispatch).toHaveBeenCalledWith(setIsPreloadActionCreator(false));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+});
